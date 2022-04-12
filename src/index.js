737e839dfe0a0cab0000bd65ad53d9a692bff502
mld-njupt <you@1761311608@qq.com>
2022-04-12 20:05:02 +0800

    complete load gltf

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-import './index.css'
+import * as THREE from "three";
+import OrbitControls from "three-orbitcontrols";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import "./index.css";
+
+//init
+//PerspectiveCamera透视投影相机
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
+camera.position.set(0, 0, 200);
+const light = new THREE.PointLight(0xffffff, 1);
+camera.add(light);
+
+//antialias:true 增加抗锯齿效果
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// const container=document.getElementById("container")
+// container.appendChild(renderer.domElement)
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+scene.add(new THREE.AmbientLight(0x222222));
+scene.add(camera);
+
+//轨道控制器控制场景的旋转、平移、缩放
+const controls = new OrbitControls(camera, renderer.domElement);
+const clock = new THREE.Clock();
+//获取时间差
+const elta = clock.getDelta();
+controls.update(elta);
+const gltfLoader = new GLTFLoader();
+const url =
+  // eslint-disable-next-line no-undef
+  process.env.NODE_ENV === "development"
+    ? "/api/react/Model.gltf"
+    : "http://103.118.40.123:9999/react/Model.gltf";
+gltfLoader.load(url, function (gltf) {
+    scene.add(gltf.scene);
+    renderer.render( scene, camera );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 /* eslint-disable no-undef */
 const path = require("path");
-const HtmlWebpackPlugin=require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode:'development',
+  mode: "development",
   entry: "./src/index.js",
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
+    // 设置代理
+    proxy: {
+      // 将本地 /api2/xxx 代理到 localhost:6666/xxx，通常用这个，/api2仅作为代理转发的标识
+      "/api": {
+        target: "http://103.118.40.123:9999",
+        changeOrigin: true,
+        pathRewrite: {
+          "/api": "",
+        },
+      },
+    },
   },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean:true
+    clean: true,
   },
   module: {
     rules: [
