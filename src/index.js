5ea0430ed572f3d5d2ccfa8bcd244ddb8aaae65f
mld-njupt <77198035+mld-njupt@users.noreply.github.com>
2022-04-13 10:56:23 +0800

    complete load axis

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
-import axisStyle from "../axis.json";
-import OrbitControls from "three-orbitcontrols";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import "./index.css";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
 
@@ -40,13 +40,6 @@ function ThreeScene(
   this.scene.add(this.camera);
   this.controls = new OrbitControls(this.camera, this.renderer.domElement);
   this.clock = new THREE.Clock();
-  this.gltfLoader = new GLTFLoader();
-  this.fontLoader = new FontLoader();
-  this.url =
-    // eslint-disable-next-line no-undef
-    process.env.NODE_ENV === "development"
-      ? "/api/react/Model.gltf"
-      : "http://103.118.40.123:9999/react/Model.gltf";
 }
 ThreeScene.prototype.render = function () {
   //获取时间差
@@ -60,7 +53,7 @@ ThreeScene.prototype.render = function () {
 function inheritPrototype(subClass, superClass) {
   function F() {}
   F.prototype = superClass.prototype;
-  subClass.prototype = F.prototype;
+  subClass.prototype = new F();
   subClass.prototype.constructor = subClass;
 }
 function MainScene(
@@ -78,11 +71,17 @@ function MainScene(
     rendererSize,
     elementId
   );
+  this.gltfLoader = new GLTFLoader();
 }
 inheritPrototype(MainScene, ThreeScene);
 MainScene.prototype.loadGLTF = function () {
+  const url =
+    // eslint-disable-next-line no-undef
+    process.env.NODE_ENV === "development"
+      ? "/api/react/Model.gltf"
+      : "http://103.118.40.123:9999/react/Model.gltf";
   const _this = this;
-  this.gltfLoader.load(this.url, function (gltf) {
+  this.gltfLoader.load(url, function (gltf) {
     _this.scene.add(gltf.scene);
     _this.render();
   });
@@ -103,12 +102,34 @@ function AxisScene(
     rendererSize,
     elementId
   );
+  this.fontLoader = new FontLoader();
 }
-inheritPrototype(MainScene, AxisScene);
+inheritPrototype(AxisScene, ThreeScene);
+AxisScene.prototype.loadAXIS = function () {
+  const dir1 = new THREE.Vector3(1, 0, 0); //创建箭头向量
+  const dir2 = new THREE.Vector3(0, 1, 0);
+  const dir3 = new THREE.Vector3(0, 0, 1);
+  const origin = new THREE.Vector3(0, 0, 0);
+  const hex1 = 0xffff00;
+  const hex2 = 0xeb4334;
+  const hex3 = 0x35aa53;
+  //箭头指示
+  this.arrowHelper1 = new THREE.ArrowHelper(dir1, origin, 20, hex1, 5, 2);
+  this.arrowHelper2 = new THREE.ArrowHelper(dir2, origin, 20, hex2, 5, 2);
+  this.arrowHelper3 = new THREE.ArrowHelper(dir3, origin, 20, hex3, 5, 2);
+  this.scene.add(this.arrowHelper1); //面向右边
+  this.scene.add(this.arrowHelper2); //面向头
+  this.scene.add(this.arrowHelper3); //面向眼睛
+};
 AxisScene.prototype.loadFONT = function () {
+  const url =
+    // eslint-disable-next-line no-undef
+    process.env.NODE_ENV === "development"
+      ? "/api/react/helvetiker_bold.typeface.json"
+      : "http://103.118.40.123:9999/react/helvetiker_bold.typeface.json";
   const _this = this;
-  this.fontLoader.load(axisStyle, function (font) {
-    const geometry = new THREE.TextGeometry("N", {
+  this.fontLoader.load(url, function (font) {
+    const geometry = new TextGeometry("N", {
       //“N是要加载的字”
       font: font,
       size: 5,
@@ -123,14 +144,30 @@ AxisScene.prototype.loadFONT = function () {
     const fontMaterial = new THREE.MeshLambertMaterial({
       color: 0xeb4334,
     });
-    const fontModel = new THREE.Mesh(geometry, fontMaterial);
-    fontModel.position.x = -2; //调整一下N的位置
 
-    _this.scene.add(fontModel);
-    _this.render();
+    _this.fontModel = new THREE.Mesh(geometry, fontMaterial);
+    _this.fontModel.position.x = -2; //调整一下N的位置
+
+    _this.scene.add(_this.fontModel);
   });
 };
-const mainScene = new ThreeScene(
+// //重写父构造函数render函数
+AxisScene.prototype.render = function (mainControls) {
+  requestAnimationFrame(this.render.bind(this, mainControls));
+  //让场景二的三个箭头随着场景一的相机旋转情况旋转
+  this.arrowHelper1.rotation.y = -mainControls.getAzimuthalAngle();
+  this.arrowHelper1.rotation.x = -mainControls.getPolarAngle();
+  this.arrowHelper2.rotation.x = -mainControls.getPolarAngle();
+  this.arrowHelper3.rotation.z = mainControls.getAzimuthalAngle();
+  this.arrowHelper3.rotation.x = 1.57079637 - mainControls.getPolarAngle();
+  /* fontModel是N，让它随着arrowHelper3转动而转动
+   */
+  this.fontModel.rotation.x = -(mainControls.getPolarAngle() - 1.57079637);
+  this.fontModel.position.z = -23 * Math.sin(mainControls.getPolarAngle());
+  this.fontModel.position.y = 23 * Math.cos(mainControls.getPolarAngle());
+  this.renderer.render(this.scene, this.camera);
+};
+const mainScene = new MainScene(
   { x: 0, y: 0, z: 200 },
   new THREE.PointLight(0xffffff, 1),
   new THREE.AmbientLight(0x222222),
@@ -146,89 +183,5 @@ const axisScene = new AxisScene(
   "axis-wrap"
 );
 axisScene.loadFONT();
-// //初始化主容器
-
-// //PerspectiveCamera透视投影相机
-// const camera = new THREE.PerspectiveCamera(
-//   40,
-//   window.innerWidth / window.innerHeight,
-//   1,
-//   1000
-// );
-// camera.position.set(0, 0, 200);
-// const light = new THREE.PointLight(0xffffff, 1);
-// camera.add(light);
-
-// //antialias:true 增加抗锯齿效果
-// const renderer = new THREE.WebGLRenderer({ antialias: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.setPixelRatio(window.devicePixelRatio);
-
-// // const container=document.getElementById("container")
-// // container.appendChild(renderer.domElement)
-// document.body.appendChild(renderer.domElement);
-
-// const scene = new THREE.Scene();
-// scene.add(new THREE.AmbientLight(0x222222));
-// scene.add(camera);
-
-// //轨道控制器控制场景的旋转、平移、缩放
-// const controls = new OrbitControls(camera, renderer.domElement);
-// const clock = new THREE.Clock();
-
-// const gltfLoader = new GLTFLoader();
-// const url =
-//   // eslint-disable-next-line no-undef
-//   process.env.NODE_ENV === "development"
-//     ? "/api/react/Model.gltf"
-//     : "http://103.118.40.123:9999/react/Model.gltf";
-// gltfLoader.load(url, function (gltf) {
-//   scene.add(gltf.scene);
-//   renderMain();
-// });
-
-// //渲染主场景
-// function renderMain() {
-//   //获取时间差
-//   const elta = clock.getDelta();
-//   controls.update(elta);
-//   requestAnimationFrame(renderMain);
-//   renderer.render(scene, camera);
-// }
-
-// //坐标轴camera scene 等
-// const axisCamera = new THREE.PerspectiveCamera(
-//   40,
-//   window.innerWidth / window.innerHeight,
-//   1,
-//   1000
-// );
-// axisCamera.position.set(30, 50, 100);
-// const axisScene = new THREE.Scene();
-// const axisRenderer = new THREE.WebGLRenderer({
-//   antialias: true,
-// });
-// axisRenderer.setPixelRatio(window.devicePixelRatio);
-// axisRenderer.setSize(window.innerWidth / 5, window.innerHeight / 5);
-
-// //控制坐标轴旋转
-// controls.addEventListener("change", function () {
-//   const cameraPostion = camera.position; // 大场景相机位置
-//   const vector = cameraPostion.clone().sub(controls.target); //
-//   const mu =
-//     Math.sqrt(
-//       Math.pow(vector.x, 2) + Math.pow(vector.y, 2) + Math.pow(vector.z, 2)
-//     ) / 2; // 求模，除以2是因为z轴上的值不够，相机的位置处于盒子里边
-//   const axisCameraPostion = new THREE.Vector3(
-//     vector.x / mu,
-//     vector.y / mu,
-//     vector.z / mu
-//   );
-//   axisCamera.position.set(
-//     axisCameraPostion.x,
-//     axisCameraPostion.y,
-//     axisCameraPostion.z
-//   );
-//   axisCamera.lookAt(axisScene.position);
-//   axisCamera.updateProjectionMatrix();
-// });
+axisScene.loadAXIS();
+axisScene.render(mainScene.controls);
